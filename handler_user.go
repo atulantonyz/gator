6b9f53c4c596ab21a168f84ccc80a99ef411e344
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2025-05-27 00:49:06 -0400

    completed CH2: L2 & L3

--- handler_user.go
+++ handler_user.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/atulantonyz/gator/internal/database"
+	"github.com/google/uuid"
+	"os"
+	"time"
 )
 
 func handlerLogin(s *state, cmd command) error {
@@ -10,12 +15,42 @@ func handlerLogin(s *state, cmd command) error {
 	}
 
 	name := cmd.Args[0]
-
-	err := s.cfg.SetUser(name)
+	_, err := s.db.GetUser(context.Background(), name)
+	if err != nil {
+		fmt.Println("User does not exist, cannot login!")
+		os.Exit(1)
+	}
+	err = s.cfg.SetUser(name)
 	if err != nil {
 		return fmt.Errorf("couldn't set current user: %w", err)
 	}
 
 	fmt.Println("User has been set to " + cmd.Args[0])
+	return nil
+}
+
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage: %s <name>", cmd.Name)
+	}
+	name := cmd.Args[0]
+	userParams := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+	}
+	user, err := s.db.CreateUser(context.Background(), userParams)
+	if err != nil {
+		fmt.Println("User with given name already exists")
+		os.Exit(1)
+	}
+	err = s.cfg.SetUser(name)
+	if err != nil {
+		return err
+	}
+	fmt.Println("User was created")
+	fmt.Printf("id: %v\ncreated_at: %v\nupdated_at: %v\nname: %s\n", user.ID, user.CreatedAt, user.UpdatedAt, user.Name)
+
 	return nil
 }
--- main.go
+++ main.go
@@ -1,12 +1,16 @@
 package main
 
+import _ "github.com/lib/pq"
 import (
+	"database/sql"
 	"github.com/atulantonyz/gator/internal/config"
+	"github.com/atulantonyz/gator/internal/database"
 	"log"
 	"os"
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -15,12 +19,17 @@ func main() {
 	if err != nil {
 		log.Fatalf("Error reading config: %v", err)
 	}
+	dbUrl := cfg.Db_url
+	db, err := sql.Open("postgres", dbUrl)
+	dbQueries := database.New(db)
 	var app_state state
 	app_state.cfg = &cfg
+	app_state.db = dbQueries
 	app_commands := commands{
 		map[string]func(*state, command) error{},
 	}
 	app_commands.register("login", handlerLogin)
+	app_commands.register("register", handlerRegister)
 	cmd_line_args := os.Args
 	if len(cmd_line_args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
