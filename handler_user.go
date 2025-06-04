201fc490366abfb3da9b36bdd96fcea0789b0315
atulantonyz <40585728+atulantonyz@users.noreply.github.com>
2025-06-04 00:46:56 -0400

    created feeds table and query to create new feed

--- handler_user.go
+++ handler_user.go
@@ -91,3 +91,30 @@ func handlerAgg(s *state, cmd command) error {
 	fmt.Println(*rssfeed)
 	return nil
 }
+
+func handlerAddFeed(s *state, cmd command) error {
+	if len(cmd.Args) != 2 {
+		fmt.Printf("usage: %s <name> <url>\n", cmd.Name)
+		os.Exit(1)
+	}
+	name := cmd.Args[0]
+	url := cmd.Args[1]
+	user, err := s.db.GetUser(context.Background(), s.cfg.Current_user_name)
+	feedParams := database.CreateFeedParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      name,
+		Url:       url,
+		UserID:    user.ID,
+	}
+	feed, err := s.db.CreateFeed(context.Background(), feedParams)
+	if err != nil {
+		return err
+	}
+	fmt.Println("Feed was created")
+	fmt.Printf("id: %v\ncreated_at: %v\nupdated_at: %v\nname: %s\nurl: %s\nuser_id: %v\n", feed.ID, feed.CreatedAt, feed.UpdatedAt,
+		feed.Name, feed.Url, feed.UserID)
+	return nil
+
+}
--- main.go
+++ main.go
@@ -33,6 +33,7 @@ func main() {
 	app_commands.register("reset", handlerReset)
 	app_commands.register("users", handlerUsers)
 	app_commands.register("agg", handlerAgg)
+	app_commands.register("addfeed", handlerAddFeed)
 	cmd_line_args := os.Args
 	if len(cmd_line_args) < 2 {
 		log.Fatalf("Usage: cli <command> [args...]")
